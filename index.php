475b01ff4cf69d450c14b1ea1e61a7c0f949ce90
Jerssao <ojefferson.af@gmail.com>
2026-07-22 21:12:25 -0300

    Criação do Style.css

--- index.php
+++ index.php
@@ -7,42 +7,54 @@ if (!isset($_SESSION['tarefas'])) {
 }
 
 require_once 'funcoes.php';
+#require_once '/./style.css';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
-<body>
-    <h1>Tarefas</h1>
-    <form action="salvar.php" method="POST">
-        <label for="tarefa">Nova tarefa:</label>
-        <br>
-        <input type="text" name="titulo" id="titulo" placeholder="descreva a tarefa" required>
 
-        <button type="submit">Salvar</button>
+<body>
+    <form class="form" action="salvar.php" method="POST">
+        <div class="flex"></div>
+        <h1>
+            <p class="title">Tarefas</p>
+        </h1>
+        <label class="message" for="tarefa">Nova tarefa:</label>
+
+        <br>
+        
+        <label>
+            <input class="input" type="text" name="titulo" id="titulo" placeholder="" required="">
+            <span>Informe a tarefa</span>
+        </label>
+        <button type="submit" class="submit">Salvar</button>
     </form>
 
     <hr>
     <h2>Lista de tarefas cadastradas</h2>
 
-<pre>
+    <pre>
 <?php
 listarTarefas();
 ?>
 </pre>
-<h2>Resumo</h2>
+    <h2>Resumo</h2>
 
- <?php
+    <?php
     $resumo = resumo($_SESSION['tarefas']);
 
     echo "Total: " . $resumo['total'] . "\n";
     echo "Pendentes: " . $resumo['pendentes'] . "\n";
     echo "Concluídas: " . $resumo['concluidas'] . "\n";
-?>
+    ?>
 
 
 </body>
+
 </html>
