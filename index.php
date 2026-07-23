ace6cd920d8330579d86454481cfa4e2710429b5
Jerssao <ojefferson.af@gmail.com>
2026-07-22 21:30:05 -0300

    Estilizando form e fundo da tela

--- index.php
+++ index.php
@@ -19,41 +19,42 @@ require_once 'funcoes.php';
     <title>Document</title>
 </head>
 
-<body>
-    <form class="form" action="salvar.php" method="POST">
-        <div class="flex"></div>
-        <h1>
-            <p class="title">Tarefas</p>
-        </h1>
-        <label class="message" for="tarefa">Nova tarefa:</label>
+<body class="containerfundo">
+    <div class="container">
+        <form class="form" action="salvar.php" method="POST">
+            <div class="flex"></div>
+            <h1>
+                <p class="title">Tarefas</p>
+            </h1>
+            <label class="message" for="tarefa">Nova tarefa:</label>
 
-        <br>
-        
-        <label>
-            <input class="input" type="text" name="titulo" id="titulo" placeholder="" required="">
-            <span>Informe a tarefa</span>
-        </label>
-        <button type="submit" class="submit">Salvar</button>
-    </form>
+            <br>
 
-    <hr>
-    <h2>Lista de tarefas cadastradas</h2>
+            <label>
+                <input class="input" type="text" name="titulo" id="titulo" placeholder="" required="">
+                <span>Informe a tarefa</span>
+            </label>
+            <button type="submit" class="submit">Salvar</button>
+        </form>
+
+        <hr>
+        <h2>Lista de tarefas cadastradas</h2>
 
     <pre>
 <?php
 listarTarefas();
 ?>
-</pre>
-    <h2>Resumo</h2>
+    </pre>
+        <h2>Resumo</h2>
 
-    <?php
-    $resumo = resumo($_SESSION['tarefas']);
+        <?php
+        $resumo = resumo($_SESSION['tarefas']);
 
-    echo "Total: " . $resumo['total'] . "\n";
-    echo "Pendentes: " . $resumo['pendentes'] . "\n";
-    echo "Concluídas: " . $resumo['concluidas'] . "\n";
-    ?>
-
+        echo "Total: " . $resumo['total'] . "\n";
+        echo "Pendentes: " . $resumo['pendentes'] . "\n";
+        echo "Concluídas: " . $resumo['concluidas'] . "\n";
+        ?>
+    </div>
 
 </body>
 
